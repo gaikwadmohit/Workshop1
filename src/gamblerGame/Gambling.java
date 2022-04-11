e352af18cbdec6644d5efe587174f47d13df1612
Mohit Gaikwad <97322025+gaikwadmohit@users.noreply.github.com>
2022-04-11 21:18:26 +0530

    UC1-Initialize Variable

--- src/gamblerGame/Gambling.java
+++ src/gamblerGame/Gambling.java
@@ -1,11 +1,13 @@
 package gamblerGame;'
 
 public class Gambling {
-	
+	static int stake = 100;
+	static int bet = 1;
+
 	public static void main(String[] args) {
 
-		System.out.println("gambler starts the game");
-		
+		System.out.println("gambler starts with stake " + stake);
+		System.out.println("Gambler bet per stake " + bet);
 	}
 
 }
